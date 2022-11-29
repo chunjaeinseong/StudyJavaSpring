5369a2b9a6f94709818b52c58e99925941f1ff64
jmkim <sir_jm@naver.com>
2022-11-29 15:49:33 +0900

    202211291550 test code commit

--- src/main/java/com/chunjae/chunjaefaq/controller/IndexController.java
+++ src/main/java/com/chunjae/chunjaefaq/controller/IndexController.java
@@ -1,11 +1,18 @@
 package com.chunjae.chunjaefaq.controller;
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String main() {
+    public String DefaultMain() {
         return "sample";
+    } 
+
+    @GetMapping("/get")
+    public String Get() {
+        return "gettest";
     } 
 }
