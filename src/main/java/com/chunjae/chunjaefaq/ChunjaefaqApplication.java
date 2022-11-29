b696a48d8be8803a41f9696ff96f6c1ad249c8f2
jmkim <sir_jm@naver.com>
2022-11-29 15:19:46 +0900

    20221129 init

---
+++ src/main/java/com/chunjae/chunjaefaq/ChunjaefaqApplication.java
@@ -0,0 +1,13 @@
+package com.chunjae.chunjaefaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChunjaefaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChunjaefaqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chunjae/chunjaefaq/ChunjaefaqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunjae.chunjaefaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChunjaefaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
